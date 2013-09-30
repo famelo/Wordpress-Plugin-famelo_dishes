34d6a9e31d106fb3d3f8b0dcd0d8dd26b4fcfe32
Marc Neuhaus <apocalip@gmail.com>
2013-09-30 17:27:17 +0200

    Fixed content in single-dish.php

--- templates/single-dish.php
+++ templates/single-dish.php
@@ -1,16 +1,9 @@
 <?php get_header(); ?>
-    <?php
-    $mypost = array( 'post_type' => 'pluginname', );
-    $loop = new WP_Query( $mypost );
-    ?>
-    <?php while ( $loop->have_posts() ) : $loop->the_post();?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header>
-                <?php the_post_thumbnail(); ?>
-                <?php the_title(); ?>
-            </header>
-            <?php the_content(); ?>
-        </article>
-    <?php endwhile; ?>
-<?php wp_reset_query(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header>
+        <?php the_post_thumbnail(); ?>
+        <?php the_title(); ?>
+    </header>
+    <?php the_content(); ?>
+</article>
 <?php get_footer(); ?>
